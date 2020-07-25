4607f691e26a71c4ffd6a6279bc577706c19cd45
Ramon Audain <raudain123@gmail.com>
2020-07-25 04:02:39 +0000

    still trying

--- main.js
+++ main.js
@@ -1,26 +1,44 @@
 
 
-Array.prototype.flatten = function(n){
-  if (n < 1 || !n) return;
+Array.prototype.flatten = function(n, array = []){
+  if (n < 1) return array;
+  //let array;
   
-  //else if (n === 1) return this.reduce((acc, val) => acc.concat(val), []);
+  if (n >= 1 || !n) array = this.reduce((acc, val) => Array.isArray(val) ? acc.concat(val) : acc.concat(val), []);
+  console.log(n, array)
+  // else{
+  // const stack = [...this];
+  // const res = [];
+
+  // while (n--){
+  //   const next = stack.pop();
+
+  //   if (Array.isArray(next)) stack.push(...next);
+  //   else res.push(next)
+  // }
   
-  else return this.reduce((acc, val) => {
-    console.log(acc)
-    if (Array.isArray(val)) {
-      console.log(n)
-      n--;
-      return acc.concat(this.flatten(n));
-    }
+  // console.log(stack, res)
+  // return this.flatten(n-1);
+  
+  // else return array = this.reduce((acc, val) => {
+  //   console.log(val)
+  //   if (Array.isArray(val)) {
+  //     //console.log(n)
+  //     acc.concat(val);
+  //     //n--;
+  //     return this.flatten(n-1)
+  //   }
     
-    else {
-      console.log(acc)
-      acc.concat(val)
-    }
-  }, []);
+  //   else {
+  //     // console.log(acc)
+  //     return acc.concat(val)
+  //   }
+  // }, []);
+  // console.log(array)
+  return array = this.flatten(n-1, array)
 }
 
 const arr = [[1,2], 6, [3,[4,5]]] 
 
 //console.log(arr.flatten())
-arr.flatten(2)
+arr.flatten()
